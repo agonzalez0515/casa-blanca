c9c373711f9b1b3d81107845ef246e302a6f6df6
agonzalez0515 <agonzalez0515@gmail.com>
2017-02-17 23:11:03 -0800

    add contact form and pony gem to send emails to user from customer

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,11 +1,8 @@
+require 'pony'
+
 get '/' do
   erb :index
 end
-
-post '/' do
-
-end
-
 
 get '/portfolio' do
   erb :portfolio
@@ -21,4 +18,41 @@ end
 
 get '/contact' do
   erb :contact
+end
+
+post '/contact' do
+  configure_pony
+  name = params[:name]
+  sender_email = params[:email]
+  message = params[:message]
+  logger.error params.inspect
+
+  begin
+    Pony.mail(
+      :from => "#{name}, <#{sender_email}>",
+      :to => 'the-sad-clown@hotmail.com',
+      :subject =>"#{name} has contacted you",
+      :body => "#{message}, #{sender_email}",
+    )
+    erb :'_success'
+    rescue
+      @exception = $!
+      erb :'_boom'
+    end
+  end
+
+def configure_pony
+  Pony.options = {
+    :via => :smtp,
+    :via_options => {
+      :address              => 'smtp.gmail.com',
+      :port                 => '587',
+      :user_name            => ENV['TEMP_USERNAME'],
+      :password             => ENV['TEMP_PASSWORD'],
+      :authentication       => :plain,
+      :enable_starttls_auto => true,
+      :domain               => 'localhost.localdomain'
+    }
+  }
+
 end
